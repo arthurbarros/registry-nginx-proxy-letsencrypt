28ce4b91272d36e1d5a164a0c24eaac6d26894be
Arthur Barros <arthbarros@gmail.com>
2016-06-13 15:05:33 -0300

    Added README.md docker-compose.yml run.sh volumes/nginx-vhost.d/docker-registry.conf volumes/nginx-vhost.d/domain_name volumes/nginx-vhost.d/registry.arthurbarros.com volumes/registry/config/config.yml.

--- README.md
+++ README.md
@@ -5,12 +5,21 @@ This repository aim to make deploy a docker registry easy with Let's Encrypt and
 ### Running
 
 ```bash
-mkdir -p /opt/docker/
-cd ~
+#Docker Registry data directory
+mkdir -p /opt/docker/registry-data/lib/registry
 
+#This project
 git clone git@github.com:arthurbarros/registry-nginx-proxy-letsencrypt.git /opt/docker/registry-nginx-proxy-letsencrypt/
 
+#direcotry where registry users will be stored
 cd /opt/docker/registry-nginx-proxy-letsencrypt/volumes/htpasswd
-htpasswd -c docker-registry.htpasswd user1
+htpasswd -c `your_own_domain.com` `user_name`
+
+cd /opt/docker/registry-nginx-proxy-letsencrypt/
+sh run.sh my_awesome_domain_name.com my_valid@email_address.com
+
+docker-compose up -d
 ```
+
+
 
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+mv /opt/docker/registry-nginx-proxy-letsencrypt/volumes/nginx-vhost.d/domain_name /opt/docker/registry-nginx-proxy-letsencrypt/volumes/nginx-vhost.d/"$1"
+sed -i -e "s/{{domain_name}}/$1/g" /opt/docker/registry-nginx-proxy-letsencrypt/volumes/registry/config/config.yml
+sed -i -e "s/{{http_secret}}/$NEW_UUID/g" /opt/docker/registry-nginx-proxy-letsencrypt/volumes/registry/config/config.yml
+sed -i -e "s/{{domain_name}}/$1/g" /opt/docker/registry-nginx-proxy-letsencrypt/volumes/nginx-vhost.d/docker-registry.conf
+sed -i -e "s/{{domain_name}}/$1/g" /opt/docker/registry-nginx-proxy-letsencrypt/docker-compose.yml
+sed -i -e "s/{{letsencrypt_email}}/$2/g" /opt/docker/registry-nginx-proxy-letsencrypt/docker-compose.yml
+
+echo "done"
