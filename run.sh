5b0998ccccb13aa84ff0231c372d1b71dc45aa93
Arthur Barros <arthbarros@gmail.com>
2016-06-14 15:17:51 -0300

    Added run.sh.

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 mv /opt/docker/registry-nginx-proxy-letsencrypt/volumes/nginx-vhost.d/domain_name /opt/docker/registry-nginx-proxy-letsencrypt/volumes/nginx-vhost.d/"$1"
+sed -i -e "s/{{domain_name}}/$1/g" /opt/docker/registry-nginx-proxy-letsencrypt/volumes/nginx-vhost.d/"$1"
 sed -i -e "s/{{domain_name}}/$1/g" /opt/docker/registry-nginx-proxy-letsencrypt/volumes/registry/config/config.yml
 sed -i -e "s/{{http_secret}}/$NEW_UUID/g" /opt/docker/registry-nginx-proxy-letsencrypt/volumes/registry/config/config.yml
 sed -i -e "s/{{domain_name}}/$1/g" /opt/docker/registry-nginx-proxy-letsencrypt/volumes/nginx-vhost.d/docker-registry.conf
